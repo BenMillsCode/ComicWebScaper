71bd03842d5d74fa319367afc6d8c98c83cf629d
Ben Mills <bengeorgemills@gmail.com>
2020-08-25 14:45:16 +0100

    Find html tag attributes

--- CWS.py
+++ CWS.py
@@ -1,9 +1,19 @@
+#import libraries
 import requests, re
 from bs4 import BeautifulSoup
 
+#connect to archive page
 result = requests.get("https://www.questionablecontent.net/archive.php")
 
+#get the contents of the archive page
 soup = BeautifulSoup(result.content, "html.parser")
 
-for sibling in soup.b.next_siblings:
-    print(sibling.string)
+#Find all links to comics
+a_tags = soup.find_all('a')
+
+
+for tag in a_tags:
+    #Get comic name
+    print(tag.string)
+    #get link to comic
+    tag[href]
